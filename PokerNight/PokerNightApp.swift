b9c1cfd78d0be8f9a290772550faa4f89e2fc575
Logan Roche <Ljroche04@gmail.com>
2025-02-23 21:29:04 -0800

    Initial Commit

---
+++ PokerNight/PokerNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerNightApp.swift
+//  PokerNight
+//
+//  Created by Logan Roche on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct PokerNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokerNightTests/PokerNightTests.swift
@@ -0,0 +1,16 @@
+//
+//  PokerNightTests.swift
+//  PokerNightTests
+//
+//  Created by Logan Roche on 2/23/25.
+//
+
+import Testing
+
+struct PokerNightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
